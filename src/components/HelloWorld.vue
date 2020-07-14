5b0330ed4b12e918525a9b19cf82c63384f07218
wqx <790304336@qq.com>
2020-07-14 13:41:17 +0800

    增加国际化功能

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,86 +1,8 @@
 <template>
   <div class="hello" id='pdfDom' ref="pdfDom">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-     <img src="../assets/logo.png">
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <h1>{{ $t('route.dashboard') }}</h1>
+    <button @click='changeLang'>切换语言</button>
+    <button @click='HtmlToPDF'>Html装换为PDF</button>
   </div>
 </template>
 
@@ -92,9 +14,13 @@ export default {
       msg: 'Welcome to Your Vue.js App'
     }
   },
-  mounted(){
-    this.getPdf(this.$refs.pdfDom)
-    
+  methods:{
+    changeLang(){
+      this.$i18n.locale = this.$i18n.locale==='en'?'zh':'en'
+    },
+    HtmlToPDF(){
+      this.getPdf(this.$refs.pdfDom)
+    }
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,13 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import htmlToPdf from '@/utils/htmlToPdf.js'
+import i18n from './lang' 
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
 Vue.use(htmlToPdf)
-
+Vue.use(Element)
+Vue.use(Element, {
+  size: 'medium', // set element-ui default size
+  i18n: (key, value) => i18n.t(key, value)
+})
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  i18n,
   components: { App },
   template: '<App/>'
 })
